6c791b104a0592c2ade8623fd3ae272fe731061a
Abumalith <abumalith@gmail.com>
2018-01-28 11:39:16 +0100

    first commit

---
+++ learningByDoing/src/main/java/pl/rpf/learningByDoing/LearningByDoingApplication.java
@@ -0,0 +1,12 @@
+package pl.rpf.learningByDoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningByDoingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningByDoingApplication.class, args);
+	}
+}
---
+++ learningByDoing/src/test/java/pl/rpf/learningByDoing/LearningByDoingApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.rpf.learningByDoing;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearningByDoingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
